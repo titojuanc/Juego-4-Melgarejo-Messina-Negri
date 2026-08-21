0c813e953f6cb96a753f3bb0360c45579b8f0f6a
Juan Cruz <alumno26.MELGAREJO.Juan@ipm.edu.ar>
2026-08-21 02:03:07 -0300

    Creada la entrada a la mazmorra. Se entra con E

---
+++ tower-defense/scenes/EntradaCueva.gd
@@ -0,0 +1,29 @@
+extends Node3D
+
+@export_file("*.tscn") var escena_mazmorra := "res://scenes/mazmorra.tscn"
+@onready var area_interaccion: Area3D = $Area3D
+
+var jugador_en_rango := false
+var cambiando_escena := false
+
+func _ready() -> void:
+	area_interaccion.body_entered.connect(_on_area_3d_body_entered)
+	area_interaccion.body_exited.connect(_on_area_3d_body_exited)
+
+func _process(_delta: float) -> void:
+	if cambiando_escena:
+		return
+	if jugador_en_rango and Input.is_action_just_pressed("interactuar"):
+		cambiando_escena = true
+		var error := get_tree().change_scene_to_file(escena_mazmorra)
+		if error != OK:
+			cambiando_escena = false
+			push_error("No se pudo cambiar a la escena: %s" % escena_mazmorra)
+
+func _on_area_3d_body_entered(body: Node3D) -> void:
+	if body.is_in_group("Jugador"):
+		jugador_en_rango = true
+
+func _on_area_3d_body_exited(body: Node3D) -> void:
+	if body.is_in_group("Jugador"):
+		jugador_en_rango = false
